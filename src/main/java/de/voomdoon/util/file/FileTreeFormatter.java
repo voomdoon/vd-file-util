a3895c0f37404d14e981787c27a63dd7ba57ccc7
voomdoon <nilreb24-github20141105@yahoo.de>
2025-05-14 21:27:01 +0200

    feat: add FileTreeFormatter with basic output of directories
    references: #1

---
+++ src/main/java/de/voomdoon/util/file/FileTreeFormatter.java
@@ -0,0 +1,74 @@
+package de.voomdoon.util.file;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Optional;
+
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+public class FileTreeFormatter {
+
+	/**
+	 * DOCME add JavaDoc for method format
+	 * 
+	 * @param directory
+	 * @return
+	 * @since 0.1.0
+	 */
+	public String format(File directory) {
+		StringBuilder sb = new StringBuilder();
+
+		appendDirectory(sb, directory, 0, Optional.empty());
+
+		return sb.toString();
+	}
+
+	/**
+	 * DOCME add JavaDoc for method append
+	 * 
+	 * @param sb
+	 * @param directory
+	 * @param level
+	 * @param last
+	 * @since 0.1.0
+	 */
+	private void appendDirectory(StringBuilder sb, File directory, int level, Optional<Boolean> last) {
+		if (level > 0 && last.isPresent()) {
+			sb.append("\n");
+			sb.append(" ".repeat((level - 1) * 4));
+
+			if (!last.get()) {
+				sb.append("├── ");
+			} else {
+				sb.append("└── ");
+			}
+		}
+
+		sb.append(directory.getName());
+		appendFilesOfDirectory(sb, directory, level);
+	}
+
+	/**
+	 * DOCME add JavaDoc for method appendFilesOfDirectory
+	 * 
+	 * @param sb
+	 * @param directory
+	 * @param level
+	 * @since 0.1.0
+	 */
+	private void appendFilesOfDirectory(StringBuilder sb, File directory, int level) {
+		File[] files = directory.listFiles();
+		Arrays.sort(files);
+
+		for (int i = 0; i < files.length; i++) {
+			File file = files[i];
+			boolean last = (i == files.length - 1);
+			appendDirectory(sb, file, level + 1, Optional.of(last));
+		}
+	}
+}
---
+++ src/test/java/de/voomdoon/util/file/FileTreeFormatterTest.java
@@ -0,0 +1,119 @@
+package de.voomdoon.util.file;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import de.voomdoon.testing.file.TempFileExtension;
+import de.voomdoon.testing.file.TempInputDirectory;
+import de.voomdoon.testing.file.WithTempInputDirectories;
+import de.voomdoon.testing.tests.TestBase;
+
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+@ExtendWith(TempFileExtension.class)
+@WithTempInputDirectories(create = true)
+class FileTreeFormatterTest extends TestBase {
+
+	/**
+	 * @since 0.1.0
+	 */
+	private FileTreeFormatter formatter = new FileTreeFormatter();
+
+	/**
+	 * @since 0.1.0
+	 */
+	@Test
+	void test_directoryEmpty(@TempInputDirectory File directory) {
+		logTestStart();
+
+		String actual = format(directory);
+
+		assertThat(actual).contains(directory.getName());
+	}
+
+	/**
+	 * @since 0.1.0
+	 */
+	@Test
+	void test_subDirectory1(@TempInputDirectory File directory) throws Exception {
+		logTestStart();
+
+		new File(directory + "/test-sub-dir").mkdir();
+
+		String actual = format(directory);
+
+		assertThat(actual).isEqualTo(directory.getName() + "\n" + "└── test-sub-dir");
+	}
+
+	/**
+	 * @since 0.1.0
+	 */
+	@Test
+	void test_subDirectory2(@TempInputDirectory File directory) throws Exception {
+		logTestStart();
+
+		new File(directory + "/test-sub-dir1").mkdir();
+		new File(directory + "/test-sub-dir2").mkdir();
+
+		String actual = format(directory);
+
+		assertThat(actual).isEqualTo(directory.getName() //
+				+ "\n├── test-sub-dir1" //
+				+ "\n└── test-sub-dir2");
+	}
+
+	/**
+	 * @since 0.1.0
+	 */
+	@Test
+	void test_subDirectory2_order(@TempInputDirectory File directory) throws Exception {
+		logTestStart();
+
+		new File(directory + "/test-sub-dir2").mkdir();
+		new File(directory + "/test-sub-dir1").mkdir();
+
+		String actual = format(directory);
+
+		assertThat(actual).isEqualTo(directory.getName() //
+				+ "\n├── test-sub-dir1" //
+				+ "\n└── test-sub-dir2");
+	}
+
+	/**
+	 * @since 0.1.0
+	 */
+	@Test
+	void test_subSubDirectory(@TempInputDirectory File directory) throws Exception {
+		logTestStart();
+
+		new File(directory + "/test-sub-dir1/test-sub-dir2").mkdirs();
+
+		String actual = format(directory);
+
+		assertThat(actual).isEqualTo(directory.getName() //
+				+ "\n└── test-sub-dir1" //
+				+ "\n    └── test-sub-dir2");
+	}
+
+	/**
+	 * @param directory
+	 * @return
+	 * @since 0.1.0
+	 */
+	private String format(File directory) {
+		String actual = formatter.format(directory);
+
+		logger.debug("result:\n" + actual);
+
+		return actual;
+	}
+}
